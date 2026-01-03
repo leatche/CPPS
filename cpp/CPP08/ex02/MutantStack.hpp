02fc98a3ef3b93043d6b4b7f6fe0f651cd0f9642
ltcherep <ltcherep@student.42nice.fr>
2026-01-03 15:01:05 +0100

    verif n 1

--- cpp/CPP08/ex02/MutantStack.hpp
+++ cpp/CPP08/ex02/MutantStack.hpp
@@ -6,7 +6,76 @@
 /*   By: tcherepoff <tcherepoff@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 12:10:52 by tcherepoff        #+#    #+#             */
-/*   Updated: 2025/12/22 12:10:53 by tcherepoff       ###   ########.fr       */
+/*   Updated: 2025/12/26 11:32:17 by tcherepoff       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+
+#define RESET		"\033[0m"
+#define BRED		"\033[1;31m"
+#define BGREEN		"\033[1;32m"
+#define BYELLOW		"\033[1;33m"
+#define BBLUE		"\033[1;34m"
+#define BMAGENTA	"\033[1;35m"
+#define BCYAN		"\033[1;36m"
+#define BORANGE		"\033[1;38;5;208m"
+#define BPINK		"\033[1;38;5;213m"
+#define BPURPLE		"\033[1;38;5;57m"
+#define BTEAL		"\033[1;38;5;30m"
+
+#include <iostream>
+#include <cstddef>
+#include <stack>
+#include <limits>
+#include <stdexcept>
+#include <algorithm>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		typedef typename std::stack<T>::container_type::iterator iterators;
+		iterators begin()
+		{
+			return (this->c.begin());
+		}
+		iterators end()
+		{
+			return (this->c.end());
+		}
+
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		const_iterator begin() const 
+		{ 
+			return (this->c.begin());
+		}
+		const_iterator end() const
+		{ 
+			return (this->c.end()); 
+		}
+
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		reverse_iterator rbegin()
+		{ 
+			return (this->c.rbegin());
+		}
+		reverse_iterator rend()
+		{ 
+			return (this->c.rend()); 
+		}
+
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+		const_reverse_iterator rbegin() const
+		{ 
+			return (this->c.rbegin());
+		}
+		const_reverse_iterator rend() const
+		{ 
+			return (this->c.rend()); 
+		}
+};
+
+#endif
--- cpp/CPP08/ex02/main.cpp
+++ cpp/CPP08/ex02/main.cpp
@@ -6,7 +6,72 @@
 /*   By: tcherepoff <tcherepoff@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 12:11:06 by tcherepoff        #+#    #+#             */
-/*   Updated: 2025/12/22 12:11:07 by tcherepoff       ###   ########.fr       */
+/*   Updated: 2025/12/26 11:37:55 by tcherepoff       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutantStack.hpp"
+
+int main()
+{
+	//////////////////////// MAIN SUBJECT //////////////////////////////////////
+
+	std::cout << BRED << "MAIN SUBJECT" << RESET << std::endl << std::endl;
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << BCYAN << "TOP METHOD : "  << RESET << "(the element at the top of the stack) = "<< mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << BCYAN << "A POP HAS BEEN DONE :"  << RESET << " (removes the element at the top of the stack) = " << RESET << mstack.top() << std::endl;
+	std::cout <<  BGREEN << "The size is now : " << RESET << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+	MutantStack<int>::iterators it = mstack.begin();
+	MutantStack<int>::iterators ite = mstack.end();
+	++it;
+	--it;
+	std::cout << BYELLOW << "The final stack : " << RESET << std::endl;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	
+	//////////////////////// CONSTANT AND REVERSE ITERATORS ////////////////////////
+
+	std::cout << std::endl;
+	std::cout << BRED << "CONSTANT AND REVERSE ITERATORS" << RESET << std::endl << std::endl;
+
+	const MutantStack<int> constStack(mstack);
+
+	MutantStack<int>::const_iterator cit = constStack.begin();
+	MutantStack<int>::const_iterator cite = constStack.end();
+
+	std::cout << BYELLOW << "The final stack with const iterator  :" << RESET << std::endl;
+	while (cit != cite)
+	{
+		std::cout << *cit << std::endl;
+		++cit;
+	}
+
+	//////////////////////// REVERSE ITERATORS ////////////////////////
+
+	std::cout << std::endl;
+	std::cout << BRED << "REVERSE ITERATORS" << RESET << std::endl << std::endl;
+
+	MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+	MutantStack<int>::reverse_iterator rite = mstack.rend();
+
+	std::cout << BYELLOW << "The final stack reverse iterator (top to bottom) :" << RESET << std::endl;
+
+	while (rit != rite)
+	{
+		std::cout << *rit << std::endl;
+		++rit;
+	}
+
+	return (0);
+}
